5babf542f5f4c6e45ac10e58a0b8412a14a49d59
Chong Zhang <chz@google.com>
2015-01-05 11:25:39 -0800

    fix the flash caused by missing setMatrix() when surface size changes
    bug: 18773834
    
    Change-Id: I16e2f896e6fd70e9b130bb55ecefa8c2f08c684a

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -9868,7 +9868,7 @@ public class WindowManagerService extends IWindowManager.Stub
                             }
                         }
 
-                        winAnimator.setSurfaceBoundariesLocked(recoveringMemory);
+                        winAnimator.prepareSurfaceLocked(recoveringMemory);
 
                         final AppWindowToken atoken = w.mAppToken;
                         if (DEBUG_STARTING_WINDOW && atoken != null
