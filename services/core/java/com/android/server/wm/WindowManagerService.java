2acea9a8438ad36fe8e674265b4003a10e88a29f
Wale Ogunwale <ogunwale@google.com>
2015-01-23 16:05:07 -0800

    Support activities in the same process having different resources.
    Activities can be of various sizes in a multi-window environment.
    This change allows them to have override configurations that allows
    different resources to the loaded if needed.
    
    Bug: 19002213
    Change-Id: Ib2c7be0b427f5ce05e7a362bcdd496ddbc9164f0

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5244,7 +5244,12 @@ public class WindowManagerService extends IWindowManager.Stub
         }
     }
 
-    public void resizeStack(int stackId, Rect bounds) {
+    /**
+     * Re-sizes the specified stack and its containing windows.
+     * Returns a {@link Configuration} object that contains configurations settings
+     * that should be overridden due to the operation.
+     */
+    public Configuration resizeStack(int stackId, Rect bounds) {
         synchronized (mWindowMap) {
             final TaskStack stack = mStackIdToStack.get(stackId);
             if (stack == null) {
@@ -5256,6 +5261,7 @@ public class WindowManagerService extends IWindowManager.Stub
                 stack.getDisplayContent().layoutNeeded = true;
                 performLayoutAndPlaceSurfacesLocked();
             }
+            return new Configuration(stack.mOverrideConfig);
         }
     }
 
@@ -7180,7 +7186,7 @@ public class WindowManagerService extends IWindowManager.Stub
         return sw;
     }
 
-    boolean computeScreenConfigurationLocked(Configuration config) {
+    private boolean computeScreenConfigurationLocked(Configuration config) {
         if (!mDisplayReady) {
             return false;
         }
