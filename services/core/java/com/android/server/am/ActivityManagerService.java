605e14c1f052b381b2eff21f90c583340f00c887
Wale Ogunwale <ogunwale@google.com>
2015-02-07 14:04:19 -0800

    Set TaskStack config orientation based on dimensions
    The TaskStack override configuration is set based on the
    stack dimensions so we can load the most acturate resources
    for activities in the stack in a multi-window environment.
    
    Also, disabled fixed screen orientation for resizeable
    activities.
    
    Bug: 19305402
    Change-Id: I7b182554523b12f2ef77f8bbc7b16891231125bf

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -3718,6 +3718,10 @@ public final class ActivityManagerService extends ActivityManagerNative
             if (r == null) {
                 return;
             }
+            if (r.task != null && r.task.mResizeable) {
+                // Fixed screen orientation isn't supported with resizeable activities.
+                return;
+            }
             final long origId = Binder.clearCallingIdentity();
             mWindowManager.setAppOrientation(r.appToken, requestedOrientation);
             Configuration config = mWindowManager.updateOrientationFromAppTokens(
