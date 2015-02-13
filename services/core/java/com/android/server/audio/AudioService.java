7103dfc6205fb521f000c354abba0a2044d3c265
Eric Laurent <elaurent@google.com>
2015-02-12 17:28:53 -0800

    AudioService: persist volume of ring stream when ringer mode changed
    -Originally, when ringer mode changed from vibrate to normal by
     adjusting volume from 0 to 1 , volume index will not be persisted.
     After device rebooted, volume of ring stream will be restored to 0
    -Persist volume index when ringer mode changed
    
    Bug: 18762217
    
    Change-Id: I75fbeaab01d80b8bcee623795f59a9e9f16b9634

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -1778,6 +1778,15 @@ public class AudioService extends IAudioService.Stub {
                                 vss.setIndex(10, device);
                             }
                         }
+                        // Persist volume for stream ring when it is changed here
+                      final int device = getDeviceForStream(streamType);
+                      sendMsg(mAudioHandler,
+                              MSG_PERSIST_VOLUME,
+                              SENDMSG_QUEUE,
+                              device,
+                              0,
+                              mStreamStates[streamType],
+                              PERSIST_DELAY);
                     }
                 }
                 mStreamStates[streamType].mute(false);
