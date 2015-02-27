e59a093a671f3c7ba3e5736f07be3ec18a6cc65f
Alan Viverette <alanv@google.com>
2015-02-27 12:34:30 -0800

    Revert "Use ObjectAnimator for fading scrollbars, set initial duration to 1500"
    Bug: 19522833
    Bug: 19528265
    This reverts commit 72710f11ecd3baf468bf649283453cb7aafe4d74.
    
    Change-Id: I7235ae3ca53696f029cc18f19fe1d373c4f54bbf

--- graphics/java/android/graphics/drawable/Drawable.java
+++ graphics/java/android/graphics/drawable/Drawable.java
@@ -41,7 +41,6 @@ import android.graphics.Xfermode;
 import android.os.Trace;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.IntProperty;
 import android.util.StateSet;
 import android.util.TypedValue;
 import android.util.Xml;
@@ -1423,20 +1422,5 @@ public abstract class Drawable {
             default: return defaultMode;
         }
     }
-
-    /** @hide */
-    public static final IntProperty<Drawable> ALPHA = new IntProperty<Drawable>("alpha") {
-        @Override
-        public void setValue(Drawable object, int value) {
-            object.mutate();
-            object.setAlpha(value);
-            object.invalidateSelf();
-        }
-
-        @Override
-        public Integer get(Drawable object) {
-            return object.getAlpha();
-        }
-    };
 }
 
