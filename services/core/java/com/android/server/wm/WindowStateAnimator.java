2e30f09f11d050e596e5412f84adaf4c4cd3368b
Craig Mautner <cmautner@google.com>
2015-04-22 10:59:09 -0700

    Fix bad crop on clip reveal animation
    For non-fullscreen apps the dimensions of the app window must be used
    to set up the animation.
    
    Fixes bug 18392184.
    
    Change-Id: Ia1681e4a2cb74be2f820cb76ddc7c651a5e4aab6

--- services/core/java/com/android/server/wm/WindowStateAnimator.java
+++ services/core/java/com/android/server/wm/WindowStateAnimator.java
@@ -1153,8 +1153,6 @@ class WindowStateAnimator {
                     mShownAlpha *= appTransformation.getAlpha();
                     if (appTransformation.hasClipRect()) {
                         mClipRect.set(appTransformation.getClipRect());
-                        // Account for non-fullscreen windows
-                        mClipRect.offset(frame.left, frame.top);
                         if (mWin.mHScale > 0) {
                             mClipRect.left /= mWin.mHScale;
                             mClipRect.right /= mWin.mHScale;
@@ -1250,7 +1248,7 @@ class WindowStateAnimator {
         }
     }
 
-    void applyDecorRect(final Rect decorRect) {
+    private void applyDecorRect(final Rect decorRect) {
         final WindowState w = mWin;
         final int width = w.mFrame.width();
         final int height = w.mFrame.height();
