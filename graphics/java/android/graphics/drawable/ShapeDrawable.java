507e5f6c6b4778f145b553437b36d2d469f8c223
Chris Craik <ccraik@google.com>
2015-05-12 10:28:14 -0700

    Revert "Fix build - Revert "Revert "Use default constructor to get anti-aliasing"""
    Fix import
    
    This reverts commit 7ed1431c83286abc83b9e5afc45fbd21ecb777b1.
    
    Change-Id: Id93eba3ed1d47a90384ba20dea6f74d78755792b

--- graphics/java/android/graphics/drawable/ShapeDrawable.java
+++ graphics/java/android/graphics/drawable/ShapeDrawable.java
@@ -496,7 +496,7 @@ public class ShapeDrawable extends Drawable {
             if (mShapeState.mPaint != null) {
                 mShapeState.mPaint = new Paint(mShapeState.mPaint);
             } else {
-                mShapeState.mPaint = new Paint();
+                mShapeState.mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
             }
             if (mShapeState.mPadding != null) {
                 mShapeState.mPadding = new Rect(mShapeState.mPadding);
@@ -550,7 +550,7 @@ public class ShapeDrawable extends Drawable {
                 mAlpha = orig.mAlpha;
                 mShaderFactory = orig.mShaderFactory;
             } else {
-                mPaint = new Paint();
+                mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
             }
         }
 
