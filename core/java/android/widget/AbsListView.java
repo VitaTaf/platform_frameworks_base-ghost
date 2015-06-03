dac525f988dd2974c61e7cdc18c22f152eab6975
Alan Viverette <alanv@google.com>
2015-06-03 10:35:44 -0700

    Call dispatchFinishTemporaryDetach before returning a transient view
    ListView caches transient state views for reuse during layout, which calls
    dispatchStartTemporaryDetach() before caching the view. This should always
    be followed by a corresponding dispatchFinishTemporaryDetach() before
    removing the view from the cache and returning it for reuse. While this
    was the case for other types of cached views, we missed this call for
    views placed in one of the two transient view caches.
    
    Bug: 17387206
    Change-Id: I360945006558b2783c098183a7cc9bc28d05d9ab

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -2350,8 +2350,10 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                 }
             }
 
-            // Scrap view implies temporary detachment.
             isScrap[0] = true;
+
+            // Finish the temporary detach started in addScrapView().
+            transientView.dispatchFinishTemporaryDetach();
             return transientView;
         }
 
@@ -2364,6 +2366,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
             } else {
                 isScrap[0] = true;
 
+                // Finish the temporary detach started in addScrapView().
                 child.dispatchFinishTemporaryDetach();
             }
         }
