c22c4c502f977dc012fe794d91c6cd5a087acce3
Jorim Jaggi <jjaggi@google.com>
2015-06-04 16:07:57 -0700

    Skip first frame for app transitions when possible
    In most of our standard app transitions, the first frame of a
    transition results in the same contents on the screen. This is
    inefficient, as we can directly skip to the second frame of the
    transition, introduce no jank, but execute the transition 16ms
    faster.
    
    Change-Id: If58337eae5558eae3acced691ae01c769f0ec2b9

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -3427,7 +3427,8 @@ public class WindowManagerService extends IWindowManager.Stub
                     }
                     Slog.v(TAG, "Loaded animation " + a + " for " + atoken, e);
                 }
-                atoken.mAppAnimator.setAnimation(a, width, height);
+                atoken.mAppAnimator.setAnimation(a, width, height,
+                        mAppTransition.canSkipFirstFrame());
             }
         } else {
             atoken.mAppAnimator.clearAnimation();
